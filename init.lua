871d1295879abb02cda95c7928f409edb8a285a9
Red_King_Cyclops <red_king_cyclops@noreply.git.minetest.land>
2020-09-18 10:23:30 +0000

    Upload files to ''

---
+++ init.lua
@@ -0,0 +1,9 @@
+dofile(minetest.get_modpath("rocket") .. "/rocket.lua")
+dofile(minetest.get_modpath("rocket") .. "/rover.lua")
+
+--To do:
+--make other_worlds compatible
+--fix rover
+--make rover craftable
+--make new version downloadable
+--update forum topic
